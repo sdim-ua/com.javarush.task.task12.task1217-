d93a0bca9fe38d705d3d5446a23eb98f20a20377
sdim-ua <ingersol@yandex.ua>
2019-06-19 23:45:06 +0400

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+//add interfaces here - добавь интерфейсы тут
+    interface CanFly{
+        void transport();
+    }
+    
+    interface CanRun{
+        void tttj();
+    }
+    
+    interface CanSwim{
+        void ghghghgh();
+    }
+    
+
+}
